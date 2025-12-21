a9ef80edd7a4f0e9c0935c6e210a7868d6001078
carl <carlstromberg3@gmail.com>
2025-12-21 18:58:45 +0200

    added dockerfile for frontend and docker-compose with frontend and backend. Changed frontend ./vite.config.ts to make ports changable

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Change this to your desired port
+    host: '0.0.0.0', // Allow external access
+    open: true, // Open browser automatically
+  },
 })
